4df1aec65f7d289892aff077a8fdbba54114f829
27 <happysmash27@vmail.me>
2017-01-10 11:14:08 -0800

    Debugged Everything

--- problem3.js
+++ problem3.js
@@ -1,16 +1,19 @@
+window.onload = (function() {
+    console.log("Page and javascript loaded!")
+});
 var theUsername = "appacademystudent"
 var thePassword = "p@ssword"
 
 /* This function should only alert "Access granted" if theUsername is "appacademystudent" and thePassword is "pizza". If the username is incorrect, it should alert "I don't know that username.". If the username is correct but the password is wrong, it should alert "Access denied." */
 
 function checkPassword() {
-    if (theUsername == "appacademystudent") {
-        if (thePassword == "pizza") {
-            alert("Access granted.")
-        } else {
-            alert("Access denied.")
-        
-     else {
-        alert("I don't know that username.")
-     }
+  if (theUsername == "appacademystudent") {
+    if (thePassword == "pizza") {
+      alert("Access granted.")
+    } else {
+      alert("Access denied.")
+    }
+  } else {
+    alert("I don't know that username.")
+  }
 }
